90bddde9533eae9fca82025c3cadaf1e1f0fa29f
ifaeya <76180866+ifaeya@users.noreply.github.com>
2020-12-18 19:33:14 +0100

    Create client.java

---
+++ client.java
@@ -0,0 +1,31 @@
+# tp-socket
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+public class Client {
+
+    static final int port = 1200;
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("connection request");
+
+        //Create a client socket
+        Socket socket = new Socket("127.0.0.1", port);
+        System.out.println("Connection established");
+
+        // create in / out
+        BufferedReader inClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter outClient = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+        Scanner sc = new Scanner(System.in);
+        int p =sc.nextInt();
+        outClient.println(p);
+        //close in / out
+        inClient.close();
+        outClient.close();
+
+        // close client socket
+        socket.close();
+    }
+}
